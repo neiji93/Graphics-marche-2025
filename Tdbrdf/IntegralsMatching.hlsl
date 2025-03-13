02c7a34bf18ce9e15ce96d97615850cbe444f847
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-13 17:08:25 +0100

    Create IntegralsMatching.hlsl

---
+++ Tdbrdf/IntegralsMatching.hlsl
@@ -0,0 +1,15 @@
+//nowadays, integrals in many fields can match good resultd.
+//at the moment, im not talking about lattices.
+//But we must rely on entry datas for hemispheres zmpling.
+//then, big datas are correct.
+
+float fullsphereNotLatticesSampli'gForTDBRDF( ...)
+  {
+
+///just use a feflection model. this will permit us to ise lattices later.
+
+}
+
+//this method is suited for one year because
+//we can adapt its behaviour.
+void nonormals...
