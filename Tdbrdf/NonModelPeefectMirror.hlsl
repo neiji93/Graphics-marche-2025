cc5c1ad66d532f0dca5d73271245f65b28d91f61
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-14 10:36:43 +0100

    Create NonModelPeefectMirror.hlsl

---
+++ Tdbrdf/NonModelPeefectMirror.hlsl
@@ -0,0 +1,11 @@
+//reflecrions can be handled for sand
+//materials and then for marerials with
+//non pbr behaviour 
+ void propagateSpecularOrGlossyShadi'gParameters ()
+   {
+
+  checkErrorPbr(...);
+  checkError
+  checkError
+
+ }
