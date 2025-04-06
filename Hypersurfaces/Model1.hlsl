08e1b7cb3cf98c564eba93f06e0858fea43a1e86
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-06 11:43:56 +0200

    Update Model1.hlsl

--- Hypersurfaces/Model1.hlsl
+++ Hypersurfaces/Model1.hlsl
@@ -2,4 +2,7 @@
 //the good way. a non obr methods would be good if we take clearcoat 
 //bit we can deal with transmission factors although.
 //to check my stuffs on tesseract, check the weight repository 
-float fettra smissionfactordromoneray(){}
+float fettra smissionfactordromoneray(){
+//rays extra datas csn be retrieved from a collection of textures.
+//but we czn use a simple transmission esuation 
+}
