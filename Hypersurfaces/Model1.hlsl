016b45da01849eda425d327e9f3b0523566b2181
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-06 11:17:29 +0200

    Create Model1.hlsl

---
+++ Hypersurfaces/Model1.hlsl
@@ -0,0 +1,5 @@
+//rays can be zchedulled but cumulating zlbedo is not
+//the good way. a non obr methods would be good if we take clearcoat 
+//bit we can deal with transmission factors although.
+//to check my stuffs on tesseract, check the weight repository 
+float fettra smissionfactordromoneray(){}
